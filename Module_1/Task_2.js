8815f4cb27ad801459098f9c8d36335722954649
Alexey <alexey.karpina@gmail.com>
2020-04-22 18:33:52 +0300

    add Tesk_2

---
+++ Module_1/Task_2.js
@@ -0,0 +1,8 @@
+'use strict';
+const total = 100;
+const ordered = 180;
+if (total >= ordered){
+    console.log(`Заказ оформлен, с вами свяжется менеджер`);
+}else{
+    console.log(`На складе недостаточно товаров!`);
+}
