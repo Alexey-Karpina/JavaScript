5de441417248e2c07de7ee0e91fff607c304c886
Alexey <alexey.karpina@gmail.com>
2020-04-22 18:57:34 +0300

    Task_4

---
+++ Module_1/Task_4.js
@@ -0,0 +1,15 @@
+'use sctrict';
+const credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice;
+let droidsAmount = Number(prompt("Сколько дроидов вы хотите купить"));
+if (droidsAmount === null){
+    console.log("Отменено пользователем!");
+} else {
+    totalPrice = droidsAmount * pricePerDroid;
+    if (totalPrice > credits){
+        console.log("Недостаточно средств на счету!");
+    } else {
+        console.log(`Вы купили ${droidsAmount} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
+    }
+}
