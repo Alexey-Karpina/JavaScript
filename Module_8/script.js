9386ef30c9adc0b344ed8b5999ea7f3ce6ac2f50
Alexey-Karpina <alexey.karpina@gmail.com>
2020-07-15 17:15:58 +0300

    Module_8

---
+++ Module_8/script.js
@@ -0,0 +1,150 @@
+const images = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
+
+//-------------------- Рендер галереи -------------------
+
+const galleryList = document.querySelector(".js-gallery");
+const render = renderGallery(images);
+galleryList.insertAdjacentHTML("beforeend", render);
+function renderGallery(images) {
+  let result = "";
+  for (let i = 0; i < images.length; i += 1) {
+    result += `
+    <li class="gallery__item">
+        <a href="" class="gallery__link">
+            <img src="${images[i].preview}" alt="${images[i].description}" class="gallery__image" data-action="${images[i].original}">
+        </a>
+    </li>`;
+  }
+  return result;
+}
+
+// Реализация делегирования и получение ссылки на большое изображение ---------------------
+
+const modal = document.querySelector(".js-lightbox");
+const modalImg = document.querySelector(".lightbox__image");
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+galleryList.addEventListener("click", handleGalleryClick);
+function handleGalleryClick(event) {
+  let imgNode = event.target;
+  let originalUrl = getOriginalUrl(imgNode);
+  const imgNodeName = event.target.nodeName;
+
+  if (imgNodeName === "IMG") {
+    modal.classList.add("is-open");
+    modalImg.setAttribute("src", originalUrl);
+  }
+  window.addEventListener("keyup", handleEscCloseModal);
+  window.addEventListener("keydown", (event) => {
+    const key = event.code;
+    const galleryItem = event.target.closest(".gallery__item");
+    const nextItem = galleryItem.nextElementSibling;
+    const prevItem = galleryItem.previousElementSibling;
+    if(key === "ArrowRight" && nextItem !== null){
+        imgNode = nextItem.querySelector(".gallery__image");
+        originalUrl = getOriginalUrl(imgNode);
+        modalImg.setAttribute("src", originalUrl);
+        return;
+    }
+    if(key === "ArrowLeft" && prevItem !== null){
+        imgNode = prevItem.querySelector(".gallery__image");
+        originalUrl = getOriginalUrl(imgNode);
+        modalImg.setAttribute("src", originalUrl);
+        return;
+    }
+  });
+}
+function getOriginalUrl(node) {
+  return node.getAttribute("data-action");
+}
+
+//---------- Закрытие модального окна -------------
+
+const closeBtn = document.querySelector(`button[data-action="close-modal"]`);
+const modalOverlay = document.querySelector(".lightbox__overlay");
+modalOverlay.addEventListener("click", handleCloseModal);
+closeBtn.addEventListener("click", handleCloseModal);
+function handleCloseModal() {
+  modalImg.setAttribute("src", "");
+  modal.classList.remove("is-open");
+}
+modal.addEventListener("click", (event) => {
+  const modalNodeName = event.target.nodeName;
+  if (modalNodeName === "DIV") {
+    handleCloseModal();
+  }
+});
+function handleEscCloseModal(event) {
+  const key = event.code;
+  if (key === "Escape") {
+    handleCloseModal();
+  }
+}
+
+//---- Переход междк фотографиями с помощью стрелочек ---------
+
